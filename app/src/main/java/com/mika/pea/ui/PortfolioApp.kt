eaa5922152358828dc6c2a8f69b58f6c4dbfcfce
mickaelsajda-svg <mickael.sajda@gmail.com>
2025-08-16 21:39:01 +0200

    Update PortfolioApp.kt

--- app/src/main/java/com/mika/pea/ui/PortfolioApp.kt
+++ app/src/main/java/com/mika/pea/ui/PortfolioApp.kt
@@ -1,6 +1,5 @@
-package com.mika.pea.ui
-
 @file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+package com.mika.pea.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
