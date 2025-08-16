b188500562cadd30165c698d259fa663e5a25563
mickaelsajda-svg <mickael.sajda@gmail.com>
2025-08-16 21:08:18 +0200

    Update PortfolioApp.kt

--- app/src/main/java/com/mika/pea/ui/PortfolioApp.kt
+++ app/src/main/java/com/mika/pea/ui/PortfolioApp.kt
@@ -5,7 +5,7 @@ package com.mika.pea.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*          // TopAppBar est ici
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mika.pea.PortfolioViewModel
 import com.mika.pea.data.db.entity.Holding
+
 
 
 @Composable
